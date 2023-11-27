6d45fc27ca21749ebd8b9b318dd824630a5a8198
anonymousrecords <a.Records2022@gmail.com>
2023-11-27 20:41:37 +0900

    :tada: 프로젝트 초기 세팅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css';
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>goal-betting</div>;
+}
