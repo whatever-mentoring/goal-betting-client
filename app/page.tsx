09dd5ee0340c9d54082d6965a648e1310927375b
ww8007 <wshmin1234@gmail.com>
2023-12-09 20:31:44 +0900

    fix: 화살표 함수 방식으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>example page</main>;
-}
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
