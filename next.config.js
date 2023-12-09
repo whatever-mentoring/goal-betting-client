d3105ae93ca4170b506e90857ae986bfaeb19fff
ww8007 <wshmin1234@gmail.com>
2023-12-09 20:30:52 +0900

    setting: vanilla extract css 셋팅 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = withVanillaExtract(nextConfig);
