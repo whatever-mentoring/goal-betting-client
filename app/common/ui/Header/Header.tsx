9966ad5d26609c31ae2418b848f7a882a36582bf
ww8007 <wshmin1234@gmail.com>
2023-12-10 21:14:25 +0900

    style: props 네임 좀 더 직관적이게 변경

--- app/common/ui/Header/Header.tsx
+++ app/common/ui/Header/Header.tsx
@@ -7,7 +7,7 @@ import { ButtonIcon } from '../assets/Icon';
 
 interface HeaderProps {
   showBackButton?: boolean;
-  rightButton?: ReactNode;
+  appendingRightButton?: ReactNode;
   backCallback?: () => void;
 }
 
@@ -16,15 +16,15 @@ interface HeaderProps {
  * Header 컴포넌트는 페이지 상단에 위치하는 컴포넌트입니다.
  *
  * @param {boolean} showBackButton 뒤로가기 버튼을 보여줄지 여부를 결정합니다.
- * @param {ReactNode} rightButton 오른쪽에 위치할 버튼을 결정합니다.
+ * @param {ReactNode} appendingRightButton 오른쪽에 위치할 버튼을 결정합니다.
  * @param {() => void} backCallback 뒤로가기 버튼을 눌렀을 때 실행할 콜백 함수입니다.
  *
  * @example
  * <Header showBackButton />
- * <Header showBackButton title="답변하기" rightButton={<ButtonIcon onClick={() => console.log('header')} />} />
+ * <Header showBackButton title="답변하기" appendingRightButton={<ButtonIcon onClick={() => console.log('header')} />} />
  */
 
-const Header = ({ showBackButton = false, rightButton, backCallback }: HeaderProps) => {
+const Header = ({ showBackButton = false, appendingRightButton, backCallback }: HeaderProps) => {
   const router = useRouter();
   const onClickBack = () => {
     if (backCallback) backCallback();
@@ -38,7 +38,7 @@ const Header = ({ showBackButton = false, rightButton, backCallback }: HeaderPro
         )}
       </div>
 
-      <div className={headerStyles.right}>{rightButton}</div>
+      <div className={headerStyles.right}>{appendingRightButton}</div>
     </div>
   );
 };
