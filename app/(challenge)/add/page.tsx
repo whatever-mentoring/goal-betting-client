5fa71dcdc3561e93ba659ba5cd4db4860ef23d2d
ww8007 <wshmin1234@gmail.com>
2023-12-12 17:21:03 +0900

    feat: 다짐 등록 퍼널 페이지 추가

---
+++ app/(challenge)/add/page.tsx
@@ -0,0 +1,84 @@
+'use client';
+import dayjs from 'dayjs';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { useFunnel } from '../../common/hooks/funnel/useFunnel';
+import BetPage from '../ui/Bet/BetPage';
+import ChallengeInputPage from '../ui/Description/ChallengeInputPage';
+
+import PreviewPage from '../ui/Preview/PreviewPage';
+import ResultPage from '../ui/Result/ResultPage';
+import StartDatePage from '../ui/StartDate/StartDatePage';
+
+export interface ChallengeFunnelProps {
+  challenge: Challenge;
+  setChallenge: Dispatch<SetStateAction<Challenge>>;
+  onNext: () => void;
+}
+
+export interface Challenge {
+  title: string;
+  gifticon: {
+    file: File | null;
+  };
+  startDate: dayjs.Dayjs | null;
+}
+
+const ChallengeAddFunnel = () => {
+  const [Funnel, setStep] = useFunnel([
+    '다짐_입력_화면',
+    '기프티콘_업로드',
+    '다짐_시작일',
+    '다짐_등록_확인',
+    '다짐_등록_완료',
+  ]);
+
+  const [challenge, setChallenge] = useState<Challenge>({
+    title: '',
+    gifticon: {
+      file: null,
+    },
+    startDate: null,
+  });
+
+  return (
+    <Funnel>
+      <Funnel.Step name="다짐_입력_화면">
+        <ChallengeInputPage
+          challenge={challenge}
+          setChallenge={setChallenge}
+          onNext={() => setStep('기프티콘_업로드')}
+        />
+      </Funnel.Step>
+      <Funnel.Step name="기프티콘_업로드">
+        <BetPage
+          challenge={challenge}
+          setChallenge={setChallenge}
+          onNext={() => setStep('다짐_시작일')}
+        />
+      </Funnel.Step>
+      <Funnel.Step name="다짐_시작일">
+        <StartDatePage
+          challenge={challenge}
+          setChallenge={setChallenge}
+          onNext={() => setStep('다짐_등록_확인')}
+        />
+      </Funnel.Step>
+      <Funnel.Step name="다짐_등록_확인">
+        <PreviewPage
+          challenge={challenge}
+          setChallenge={setChallenge}
+          onNext={() => setStep('다짐_등록_완료')}
+        />
+      </Funnel.Step>
+      <Funnel.Step name="다짐_등록_완료">
+        <ResultPage
+          challenge={challenge}
+          setChallenge={setChallenge}
+          onNext={() => setStep('다짐_등록_완료')}
+        />
+      </Funnel.Step>
+    </Funnel>
+  );
+};
+
+export default ChallengeAddFunnel;
