6d139b1943a22e54918e2b662fd7d32c89b220fc
ww8007 <wshmin1234@gmail.com>
2023-12-13 04:33:42 +0900

    feat: 카카오 로그인 로직 추가

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import { User } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+export interface AuthUser extends User {
+  accessToken: string;
+  accessTokenExpires: number;
+  refreshToken: string;
+}
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: account.expires_at,
+          refreshToken: account.refresh_token,
+          user,
+        };
+      }
+      return {
+        ...token,
+        accessToken: '',
+        accessTokenExpires: '',
+        refreshToken: '',
+      };
+    },
+
+    async session({ session, token }) {
+      session.user = token as unknown as AuthUser;
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: '/login',
+  },
+});
+
+export { handler as GET, handler as POST };
