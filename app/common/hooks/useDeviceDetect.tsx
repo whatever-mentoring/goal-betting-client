7a38c4eaf519b1a5e0571c90063e49a2b828ec7f
ww8007 <wshmin1234@gmail.com>
2023-12-12 16:56:13 +0900

    feat: 사용자의 브라우저가 모바일 인지 판단하는customHook 추가

---
+++ app/common/hooks/useDeviceDetect.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+const useDeviceDetect = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = typeof window.navigator === 'undefined' ? '' : navigator.userAgent;
+    const mobile = Boolean(
+      userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i),
+    );
+
+    setIsMobile(mobile);
+  }, []);
+
+  return { isMobile };
+};
+
+export default useDeviceDetect;
