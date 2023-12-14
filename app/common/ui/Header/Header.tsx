15b1ace8b33fa446dec6d7f02eb4e0410f0882c6
ww8007 <wshmin1234@gmail.com>
2023-12-15 06:00:17 +0900

    feat: 헤더 뒤로가기 지정 라우팅 props 추가

--- app/common/ui/Header/Header.tsx
+++ app/common/ui/Header/Header.tsx
@@ -2,11 +2,12 @@
 
 import { useRouter } from 'next/navigation';
 import { ReactNode } from 'react';
-import { headerStyles } from './header.css';
 import { ButtonIcon } from '../assets/Icon';
+import { headerStyles } from './header.css';
 
 interface HeaderProps {
   showBackButton?: boolean;
+  backTo?: string;
   appendingRightButton?: ReactNode;
   backCallback?: () => void;
 }
@@ -16,6 +17,7 @@ interface HeaderProps {
  * Header 컴포넌트는 페이지 상단에 위치하는 컴포넌트입니다.
  *
  * @param {boolean} showBackButton 뒤로가기 버튼을 보여줄지 여부를 결정합니다.
+ * @param {string} backTo 뒤로가기 버튼을 눌렀을 때 이동할 경로를 결정합니다.
  * @param {ReactNode} appendingRightButton 오른쪽에 위치할 버튼을 결정합니다.
  * @param {() => void} backCallback 뒤로가기 버튼을 눌렀을 때 실행할 콜백 함수입니다.
  *
@@ -24,10 +26,19 @@ interface HeaderProps {
  * <Header showBackButton title="답변하기" appendingRightButton={<ButtonIcon onClick={() => console.log('header')} />} />
  */
 
-const Header = ({ showBackButton = false, appendingRightButton, backCallback }: HeaderProps) => {
+const Header = ({
+  showBackButton = false,
+  backTo,
+  appendingRightButton,
+  backCallback,
+}: HeaderProps) => {
   const router = useRouter();
   const onClickBack = () => {
     if (backCallback) backCallback();
+    if (backTo) {
+      router.push(backTo);
+      return;
+    }
     router.back();
   };
   return (
