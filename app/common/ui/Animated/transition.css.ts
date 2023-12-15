95d5fe04e49af464532ffc6298ad2c6754533cd0
ww8007 <wshmin1234@gmail.com>
2023-12-15 13:30:42 +0900

    feat: 헤더와 하단 버튼이 움직이는 UI 컴포넌트 추가 추후에 좀 더 많은 부분을 움직일 수 있도록

---
+++ app/common/ui/Animated/transition.css.ts
@@ -0,0 +1,47 @@
+import { style } from '@vanilla-extract/css';
+import getRem from '../../util/getRem';
+
+export const transitionStyles = {
+  container: style({
+    position: 'relative',
+    overflow: 'hidden',
+    height: `calc(100dvh - ${getRem(52)})`,
+  }),
+  text: style({
+    position: 'absolute',
+    width: '100%',
+    left: '50%',
+    transform: 'translateX(-70%)',
+    transition: 'transform 0.5s ease-out',
+  }),
+  buttonWrapper: style({
+    position: 'absolute',
+    left: '50%',
+    bottom: getRem(23),
+    width: '100%',
+    transform: 'translateX(-50%)',
+    transition: 'transform 0.5s ease-out',
+    padding: `0 ${getRem(20)}`,
+  }),
+
+  enterFromRight: style({
+    transform: 'translateX(150%)',
+  }),
+  exitToLeft: style({
+    transform: 'translateX(-200%)',
+  }),
+  center: style({
+    transform: 'translateX(-50%)',
+  }),
+  childrenWrapper: style({
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    top: `${getRem(52)}`,
+    left: '50%',
+    width: '100%',
+    minHeight: `calc(100dvh - ${getRem(200)})`,
+    transform: 'translateX(-50%)',
+    transition: 'transform 0.5s ease-out',
+  }),
+};
