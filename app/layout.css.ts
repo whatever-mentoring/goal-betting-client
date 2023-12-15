003887f812a0909620be8aaf3460dd140470ab30
ww8007 <wshmin1234@gmail.com>
2023-12-15 13:21:07 +0900

    design: 하단 고정 영역 영향 받는 layout으로 제거

--- app/layout.css.ts
+++ app/layout.css.ts
@@ -1,8 +1,3 @@
 import { style } from '@vanilla-extract/css';
-import getRem from './common/util/getRem';
 
-// TODO : 추후 좀 더 정리 필요
-// NOTE : 하단 고정 버튼 대응
-export const layoutStyle = style({
-  marginBottom: `${getRem(100)}`,
-});
+export const layoutStyle = style({});
