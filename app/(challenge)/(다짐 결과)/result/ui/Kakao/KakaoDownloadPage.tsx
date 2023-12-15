5c0f88fd7ea15e08069b92772ae7021fb5f02a07
ww8007 <wshmin1234@gmail.com>
2023-12-15 13:38:57 +0900

    feat: 참여자 기준 다짐 결과에 따른 결과 기프티콘 이미지 저장 페이지 추가

---
+++ app/(challenge)/(다짐 결과)/result/ui/Kakao/KakaoDownloadPage.tsx
@@ -0,0 +1,55 @@
+import useCaptureAndDownloadImage from '@/app/common/hooks/useCaptureAndDownloadImage';
+import navigationPath from '@/app/common/navigation/navigationPath';
+import BottomFixedButton from '@/app/common/ui/Button/BottomFixedButton';
+import Header from '@/app/common/ui/Header/Header';
+import Text from '@/app/common/ui/Text/Text';
+import { withPreWrapCenter } from '@/app/common/ui/common.css';
+import Image from 'next/image';
+import { useRef } from 'react';
+import { ChallengeResultFunnel } from '../../page';
+import { kakaoPageStyles } from './kakao.css';
+
+type OmitOnNext = Omit<ChallengeResultFunnel, 'onNext'>;
+interface SaverPageProps extends OmitOnNext {}
+
+const KakaoDownloadPage = ({}: SaverPageProps) => {
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  const { captureAndDownload } = useCaptureAndDownloadImage(imageRef);
+
+  const onClickDownload = () => {
+    captureAndDownload('다짐을 인증해줘!');
+  };
+
+  return (
+    <>
+      <Header showBackButton backTo={navigationPath.다짐_도전_퍼널.다짐_도전} />
+      <div className={kakaoPageStyles.headerTextWrapper}>
+        <Text.TitleH1 className={withPreWrapCenter}>{'기프티콘을\n저장해주세요'}</Text.TitleH1>
+      </div>
+
+      <div className={kakaoPageStyles.imageDownloadWrapper} ref={imageRef}>
+        <div className={kakaoPageStyles.imageWrapper}>
+          <Image
+            className={kakaoPageStyles.image}
+            src="/images/dog.png"
+            alt="Image description"
+            layout="responsive"
+            width={100}
+            height={100}
+            quality={100}
+            priority
+          />
+        </div>
+      </div>
+
+      <BottomFixedButton>
+        <BottomFixedButton.First onClick={onClickDownload} color="purple500-active">
+          <Text.ButtonL color={'white'}>다운로드하기</Text.ButtonL>
+        </BottomFixedButton.First>
+      </BottomFixedButton>
+    </>
+  );
+};
+
+export default KakaoDownloadPage;
