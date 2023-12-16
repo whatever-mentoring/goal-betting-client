cc7aa6a890bd62dd9b909b6cbfe97d58d0155f37
ww8007 <wshmin1234@gmail.com>
2023-12-17 01:15:08 +0900

    feat: 햄버거 메뉴에 들어가는 LoginDrawer UI 컴포넌트 추가

---
+++ app/common/ui/Drawer/loginDrawer.css.ts
@@ -0,0 +1,54 @@
+import { style } from '@vanilla-extract/css';
+import getRem from '../../util/getRem';
+import { vars } from '../colors.css';
+import { flexCenterStart } from '../common.css';
+
+export const loginDrawerStyles = {
+  container: style({
+    padding: `0 ${getRem(31)}`,
+  }),
+  headerTextWrapper: style([
+    {
+      display: 'flex',
+      flexDirection: 'column',
+      marginTop: `${getRem(8)}`,
+      rowGap: `${getRem(8)}`,
+    },
+  ]),
+  nicknameWrapper: style([flexCenterStart]),
+
+  linkButtonWrapper: style([
+    {
+      display: 'flex',
+      flexDirection: 'column',
+      marginTop: `${getRem(45)}`,
+      rowGap: `${getRem(8)}`,
+      boxSizing: 'border-box',
+    },
+  ]),
+
+  linkButton: style([
+    flexCenterStart,
+    {
+      columnGap: `${getRem(8)}`,
+      padding: `${getRem(6)} ${getRem(0)}`,
+      boxSizing: 'border-box',
+      borderRadius: `${getRem(5)}`,
+      cursor: 'pointer',
+      transition: 'all 0.3s ease-in-out',
+
+      selectors: {
+        '&:hover': {
+          padding: `${getRem(6)} ${getRem(16)}`,
+          backgroundColor: vars.color.grey800,
+          color: vars.color.white,
+        },
+        '&:active': {
+          padding: `${getRem(6)} ${getRem(16)}`,
+          backgroundColor: vars.color.grey800,
+          color: vars.color.white,
+        },
+      },
+    },
+  ]),
+};
