54f358cd35004face76e69803b6c20caae4f6647
ww8007 <wshmin1234@gmail.com>
2023-12-17 16:02:26 +0900

    refactor: 닉네임 입력 비지니스 로직 커스텀 훅으로 추상화

--- app/login/ui/Nickname/NicknamePage.tsx
+++ app/login/ui/Nickname/NicknamePage.tsx
@@ -2,51 +2,52 @@ import BottomFixedButton from '@/app/common/ui/Button/BottomFixedButton';
 import Header from '@/app/common/ui/Header/Header';
 import Text from '@/app/common/ui/Text/Text';
 import TextArea from '@/app/common/ui/TextArea/TextArea';
-import { headerTextWrapper } from '@/app/common/ui/common.css';
-import React from 'react';
+import { headerTextWrapper, withPreWrapCenter } from '@/app/common/ui/common.css';
+import useHandleNickname from '../../module/nickname/useHandleNickname';
 import { LoginFunnelProps } from '../../page';
 import { nicknamePageStyles } from './nickname.css';
 
 interface NicknamePageProps extends LoginFunnelProps {}
 
 const NicknamePage = ({ user, setUser, onNext }: NicknamePageProps) => {
-  // USER INTERACTION
-  // 1. 유저 > 닉네임 입력
-  const onChangeNickname = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setUser((prev) => ({ ...prev, nickname: e.target.value }));
-  };
-
-  // 2. 유저 > 다음으로
-  const isAllFilled = user.nickname.length > 0;
+  const { placeholder, isAllFilled, onChangeNickname, onSubmit } = useHandleNickname({
+    user,
+    setUser,
+    onNext,
+  });
   return (
     <>
       <Header showBackButton />
       <div className={headerTextWrapper}>
-        <Text.TitleH1>너는 이름이 뭐야?</Text.TitleH1>
+        <Text.TitleH1 className={withPreWrapCenter}>
+          {'나는 청룡이야\n너는 이름이 뭐야?'}
+        </Text.TitleH1>
       </div>
-      <div className={nicknamePageStyles.inputWrapper}>
+      <form onSubmit={onSubmit} className={nicknamePageStyles.inputWrapper}>
         <TextArea>
           <TextArea.Base
-            placeholder="이름을 입력해주세요"
+            placeholder={placeholder}
             value={user.nickname}
             onChange={onChangeNickname}
             maxRows={1}
+            rows={1}
             withCount={{
               max: 7,
             }}
+            required
           />
         </TextArea>
-      </div>
-      <BottomFixedButton>
-        <BottomFixedButton.First
-          disabled={!isAllFilled}
-          color="purple500-active"
-          width={100}
-          onClick={onNext}
-        >
-          <Text.ButtonL color={isAllFilled ? 'white' : 'grey400'}>다음으로</Text.ButtonL>
-        </BottomFixedButton.First>
-      </BottomFixedButton>
+        <BottomFixedButton>
+          <BottomFixedButton.First
+            disabled={!isAllFilled}
+            color="purple500-active"
+            width={100}
+            type="submit"
+          >
+            <Text.ButtonL color={isAllFilled ? 'white' : 'grey400'}>다음으로</Text.ButtonL>
+          </BottomFixedButton.First>
+        </BottomFixedButton>
+      </form>
     </>
   );
 };
