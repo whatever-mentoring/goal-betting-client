6c2d1314e5c1e70b8bfd555f5d4953282d7ddacd
ww8007 <wshmin1234@gmail.com>
2023-12-19 00:46:04 +0900

    feat: cors용 proxy rewrites 설정 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/v1/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/v1/:path*`,
+      },
+    ];
+  },
+};
 
 module.exports = withVanillaExtract(nextConfig);
