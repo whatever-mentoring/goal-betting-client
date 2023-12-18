6ac5feec6c625348bc34a6e77771cbddf944080e
ww8007 <wshmin1234@gmail.com>
2023-12-19 00:58:40 +0900

    feat: 챌린지 생성 api 연동 및 비지니스 로직 customHook으로 이동

---
+++ app/(challenge)/(다짐 생성)/module/api/challenge.ts
@@ -0,0 +1,53 @@
+import { withSessionUser } from '@/app/common/api/withSessionUser';
+import client from '@/app/common/api/client';
+import { useMutation } from '@tanstack/react-query';
+
+export interface Json {
+  isSuccess: boolean;
+  data: Data;
+  errorResponse: unknown;
+}
+export interface Data {
+  id: number;
+  type: string;
+  content: Content;
+  threshold: Threshold;
+  startDate: StartDate;
+  endDate: EndDate;
+}
+export interface Content {
+  value: string;
+}
+export interface Threshold {
+  value: number;
+}
+export type StartDate = number[];
+
+export type EndDate = number[];
+
+interface RequestInterface {
+  postData: {
+    type: 'FREE' | 'BILLING';
+    content: string;
+    startDate: string;
+    endDate: string;
+  };
+}
+
+const postChallengeAPI = withSessionUser(async ({ postData }: RequestInterface) => {
+  const { data } = await client<Json>({
+    method: 'post',
+    url: '/v1/api/goal',
+    data: postData,
+  });
+  return data;
+});
+
+const POST_CHALLENGE_KEY = () => ['challenge'];
+
+export const usePOSTChallengeQuery = () => {
+  return useMutation({
+    mutationKey: POST_CHALLENGE_KEY(),
+    mutationFn: postChallengeAPI,
+  });
+};
