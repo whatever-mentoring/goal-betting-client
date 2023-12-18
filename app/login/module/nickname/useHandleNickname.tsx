ece5f445bdf23b40bf59d01f1b5f3126ac6fa4db
ww8007 <wshmin1234@gmail.com>
2023-12-19 01:04:08 +0900

    feat: 유저 정보, 닉네임 변경 api 연동

--- app/login/module/nickname/useHandleNickname.tsx
+++ app/login/module/nickname/useHandleNickname.tsx
@@ -1,8 +1,21 @@
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 import { LoginFunnelProps } from '../../page';
+import { usePUTNickname } from '../api/nickname';
+import { useGETUserInfoQuery } from '../api/user';
 
 interface HandleNicknameProps extends LoginFunnelProps {}
 
 const useHandleNickname = ({ user, setUser, onNext }: HandleNicknameProps) => {
+  const { data: userInfo } = useGETUserInfoQuery();
+
+  useEffect(() => {
+    if (userInfo?.data) {
+      setUser({
+        nickname: userInfo.data.nickname.value,
+      });
+    }
+  }, [userInfo]);
   // USER INTERACTION
   // 1. 유저 > 닉네임 placeholder
   const placeholder = user.nickname.length > 0 ? user.nickname : '닉네임을 입력해주세요';
@@ -15,9 +28,14 @@ const useHandleNickname = ({ user, setUser, onNext }: HandleNicknameProps) => {
   // 2. 유저 > 다음으로
   const isAllFilled = user.nickname.length > 0;
 
+  // 3. 닉네임 저장
+  const { data } = useSession();
+  const { mutate } = usePUTNickname();
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!isAllFilled) return;
+    mutate({ putData: { nickname: user.nickname }, token: data?.user.accessToken });
     onNext();
   };
 
