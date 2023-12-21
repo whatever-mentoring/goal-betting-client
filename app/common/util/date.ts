75a180c8a2001a4e1862cb406c19771a589be44e
ww8007 <wshmin1234@gmail.com>
2023-12-21 09:53:08 +0900

    feat: date 관련 util 함수추가 1. nthDayFromStartDate 시작일로부터 몇 일인지 2. covertFormatDate string format으로 변경

--- app/common/util/date.ts
+++ app/common/util/date.ts
@@ -4,3 +4,12 @@ export const getDayPeriodToText = (startDate: dayjs.Dayjs, day: number) => {
   const endDate = startDate.add(day, 'day');
   return `${startDate.format('YYYY.MM.DD')} ~ ${endDate.format('YYYY.MM.DD')}`;
 };
+
+export const nthDayFromStartDate = (startDate: Date, date?: Date) => {
+  if (!date) return dayjs().diff(startDate, 'day') + 1;
+  else dayjs(date).diff(startDate, 'day') + 1;
+};
+
+export const covertFormatDate = (date: Date, format: string) => {
+  return dayjs(date).format(format);
+};
