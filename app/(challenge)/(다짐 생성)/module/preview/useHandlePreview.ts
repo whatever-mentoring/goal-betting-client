dcac767d7c4b015d7e4cdbee930184d6843e4680
ww8007 <wshmin1234@gmail.com>
2023-12-21 09:56:30 +0900

    feat: 다짐 생성 완료시 비지니스 로직 추가

--- app/(challenge)/(다짐 생성)/module/preview/useHandlePreview.ts
+++ app/(challenge)/(다짐 생성)/module/preview/useHandlePreview.ts
@@ -1,4 +1,8 @@
+import { GET_CHALLENGE_LIST_KEY } from '@/app/(challenge)/(다짐 도전)/module/api/challengeList';
+import navigationPath from '@/app/common/navigation/navigationPath';
+import { useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
+import { useRouter } from 'next/navigation';
 import { ChallengeAddFunnelProps } from '../../add/page';
 import { usePOSTChallengeQuery } from '../api/challenge';
 
@@ -6,17 +10,29 @@ type PickData = Pick<ChallengeAddFunnelProps, 'challenge'>;
 interface PreviewProps extends PickData {}
 
 const useHandlePreview = ({ challenge }: PreviewProps) => {
+  const router = useRouter();
   const { mutate } = usePOSTChallengeQuery();
+  const queryClient = useQueryClient();
 
   const submit = () => {
-    mutate({
-      postData: {
-        type: 'FREE',
-        content: challenge.title,
-        startDate: dayjs(challenge.startDate).toISOString(),
-        endDate: dayjs(challenge.startDate).add(7, 'day').toISOString(),
+    mutate(
+      {
+        postData: {
+          type: 'FREE',
+          content: challenge.title,
+          startDate: dayjs(challenge.startDate).toISOString(),
+          endDate: dayjs(challenge.startDate).add(7, 'day').toISOString(),
+        },
       },
-    });
+      {
+        onSuccess: (data) => {
+          queryClient.invalidateQueries({
+            queryKey: GET_CHALLENGE_LIST_KEY(),
+          });
+          router.push(navigationPath.다짐_공유_페이지(data.data.id));
+        },
+      },
+    );
   };
 
   return {
