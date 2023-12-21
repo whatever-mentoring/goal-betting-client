e814a4d3b39faae89525cc68cb910609b8e8a8ff
ww8007 <wshmin1234@gmail.com>
2023-12-21 10:04:39 +0900

    fix: api 연동에 따른 queryString 필요성에 따라 path 변경

--- app/common/navigation/navigationPath.ts
+++ app/common/navigation/navigationPath.ts
@@ -5,7 +5,9 @@ const 로그인_퍼널: Record<로그인_퍼널_Key, string> = {
   닉네임_설정: '/login?step=닉네임_설정',
 };
 
-type 다짐_생성_퍼널_Key =
+const 홈_페이지 = '/';
+
+export type 다짐_생성_퍼널_Key =
   | '다짐_입력'
   | '기프티콘_업로드'
   | '다짐_시작일'
@@ -20,24 +22,27 @@ const 다짐_생성_퍼널: Record<다짐_생성_퍼널_Key, string> = {
   다짐_등록_완료: '/add?step=다짐_등록_완료',
 };
 
-export type 다짐_도전_퍼널_Key = '다짐_도전' | '다짐_인증' | '다짐_인증_확인' | '다짐_인증_완료';
+export const 다짐_공유_페이지 = (goalId: number) => `/share/${goalId}`;
 
-const 다짐_도전_퍼널: Record<다짐_도전_퍼널_Key, string> = {
-  다짐_도전: '/',
-  다짐_인증: '/?step=다짐_인증',
-  다짐_인증_확인: '/?step=다짐_인증_확인',
-  다짐_인증_완료: '/?step=다짐_인증_완료',
-};
+export type 다짐_도전_퍼널_Key = '다짐_인증' | '다짐_인증_확인';
+
+const 다짐_도전_퍼널 = (goalId: number): Record<다짐_도전_퍼널_Key, string> => ({
+  다짐_인증: `/challenge/${goalId}?step=다짐_인증`,
+  다짐_인증_확인: `/challenge/${goalId}?step=다짐_인증_확인`,
+});
+
+const 다짐_인증_확인_페이지 = (goalId: number) => (certificationId: number) =>
+  `/challenge/${goalId}/certificate/${certificationId}`;
 
 export type 다짐_결과_퍼널_Key = '도전_결과' | '도전_결과_저장' | '도전_기프티콘';
 
-const 다짐_결과_퍼널 = {
-  도전_결과: '/result',
-  도전_결과_저장: '/result?step=도전_결과_저장',
-  도전_기프티콘: '/result?step=도전_기프티콘',
-};
+const 다짐_결과_퍼널 = (goalId: number) => ({
+  도전_결과: `/result/${goalId}/`,
+  도전_결과_저장: `/result/${goalId}/?step=도전_결과_저장`,
+  도전_기프티콘: `/result/${goalId}/?step=도전_기프티콘`,
+});
 
-const 유저_목록_페이지 = '/users';
+const 유저_목록_페이지 = (goalId: number) => `/challenge/${goalId}/users`;
 
 const 유저_설정 = {
   계정_설정: '/user/settings',
@@ -46,8 +51,11 @@ const 유저_설정 = {
 
 export default {
   로그인_퍼널,
+  홈_페이지,
   다짐_생성_퍼널,
+  다짐_공유_페이지,
   다짐_도전_퍼널,
+  다짐_인증_확인_페이지,
   다짐_결과_퍼널,
   유저_목록_페이지,
   유저_설정,
