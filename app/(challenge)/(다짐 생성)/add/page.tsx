ace9c2a2e2be739055d45b437afcd7f17563393a
ww8007 <wshmin1234@gmail.com>
2023-12-21 10:09:21 +0900

    fix: 다짐 인증 후 확인 페이지 라우팅 변경 사항 반영

--- app/(challenge)/(다짐 생성)/add/page.tsx
+++ app/(challenge)/(다짐 생성)/add/page.tsx
@@ -3,13 +3,11 @@ import RefreshCheck from '@/app/common/hooks/funnel/RefreshCheck';
 import { extractNonEmptyArrayKeys } from '@/app/common/hooks/funnel/models';
 import navigationPath from '@/app/common/navigation/navigationPath';
 import dayjs from 'dayjs';
-import { useRouter } from 'next/navigation';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { useFunnel } from '../../../common/hooks/funnel/useFunnel';
 import BetPage from '../ui/Bet/BetPage';
 import ChallengeInputPage from '../ui/Description/ChallengeInputPage';
 import PreviewPage from '../ui/Preview/PreviewPage';
-import ResultPage from '../ui/Result/ResultPage';
 import StartDatePage from '../ui/StartDate/StartDatePage';
 
 export interface ChallengeAddFunnelProps {
@@ -27,7 +25,6 @@ export interface Challenge {
 }
 
 const ChallengeAddFunnel = () => {
-  const router = useRouter();
   const [Funnel, setStep] = useFunnel(extractNonEmptyArrayKeys(navigationPath.다짐_생성_퍼널));
 
   const [challenge, setChallenge] = useState<Challenge>({
@@ -67,19 +64,8 @@ const ChallengeAddFunnel = () => {
       </Funnel.Step>
       <Funnel.Step name="다짐_등록_확인">
         <RefreshCheck onRefresh={() => setStep('다짐_입력')}>
-          <PreviewPage
-            challenge={challenge}
-            setChallenge={setChallenge}
-            onNext={() => setStep('다짐_등록_완료')}
-          />
+          <PreviewPage challenge={challenge} setChallenge={setChallenge} />
         </RefreshCheck>
-      </Funnel.Step>
-      <Funnel.Step name="다짐_등록_완료">
-        <ResultPage
-          challenge={challenge}
-          setChallenge={setChallenge}
-          onNext={() => router.push(navigationPath.다짐_도전_퍼널.다짐_도전, { scroll: false })}
-        />
       </Funnel.Step>
     </Funnel>
   );
