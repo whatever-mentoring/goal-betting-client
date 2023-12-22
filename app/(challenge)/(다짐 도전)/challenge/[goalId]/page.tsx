545b0d815cd8df629320b121aa5083edcfdb5714
ww8007 <wshmin1234@gmail.com>
2023-12-22 21:56:59 +0900

    fix: 내기 참여의 경우 페이지 이동 처리 필요로 goalId 기반 페이지로 띄우도록 수정

--- app/(challenge)/(다짐 도전)/challenge/[goalId]/page.tsx
+++ app/(challenge)/(다짐 도전)/challenge/[goalId]/page.tsx
@@ -1,58 +1,17 @@
 'use client';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 
-import RefreshCheck from '@/app/common/hooks/funnel/RefreshCheck';
-import { extractNonEmptyArrayKeys } from '@/app/common/hooks/funnel/models';
-import { useFunnel } from '@/app/common/hooks/funnel/useFunnel';
-import navigationPath, { 다짐_도전_퍼널_Key } from '@/app/common/navigation/navigationPath';
-import { Dispatch, SetStateAction, useMemo, useState } from 'react';
-import CertificationPage from '../../ui/Certification/CertificationPage';
-import CheckPage from '../../ui/Check/CheckPage';
+const ChallengeDynamicPage = dynamic(() => import('../../ui/Challenge/ChallengePage'), {
+  ssr: false,
+});
 
-interface Certification {
-  imageSrc: string | null;
-  text: string;
-}
-
-export interface ChallengeCertificationFunnelProps {
-  onNext: () => void;
-  certification: Certification;
-  setCertification: Dispatch<SetStateAction<Certification>>;
-  setStep?: (step: 다짐_도전_퍼널_Key) => void;
-}
-
-const ChallengeFunnel = ({ params }: { params: { goalId: number } }) => {
-  const funnelStep = useMemo(
-    () => extractNonEmptyArrayKeys(navigationPath.다짐_도전_퍼널(params.goalId)),
-    [],
-  );
-  const [Funnel, setStep] = useFunnel(funnelStep);
-
-  const [certification, setCertification] = useState<Certification>({
-    imageSrc: null,
-    text: '',
-  });
-
+const ChallengePage = ({ params }: { params: { goalId: number } }) => {
   return (
-    <Funnel>
-      <Funnel.Step name="다짐_인증">
-        <CertificationPage
-          onNext={() => setStep('다짐_인증_확인')}
-          certification={certification}
-          setCertification={setCertification}
-        />
-      </Funnel.Step>
-      <Funnel.Step name="다짐_인증_확인">
-        <RefreshCheck onRefresh={() => setStep('다짐_인증')}>
-          <CheckPage
-            certification={certification}
-            setCertification={setCertification}
-            params={params}
-            onNext={() => navigationPath.다짐_인증_확인_페이지(params.goalId)}
-          />
-        </RefreshCheck>
-      </Funnel.Step>
-    </Funnel>
+    <Suspense>
+      <ChallengeDynamicPage goalId={params.goalId} />
+    </Suspense>
   );
 };
 
-export default ChallengeFunnel;
+export default ChallengePage;
