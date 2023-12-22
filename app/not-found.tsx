084f303361b31eddfc2563844b120341d2a9e9e1
ww8007 <wshmin1234@gmail.com>
2023-12-23 08:23:25 +0900

    feat: NotFoundErrorPage 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,36 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import navigationPath from './common/navigation/navigationPath';
+import CenterBlank from './common/ui/Blank/CenterBlank';
+import BottomFixedButton from './common/ui/Button/BottomFixedButton';
+import Text from './common/ui/Text/Text';
+import { errorStyles } from './layout.css';
+
+const NotFoundErrorPage = () => {
+  const router = useRouter();
+
+  const onClickHome = () => {
+    router.push(navigationPath.홈_페이지);
+  };
+
+  return (
+    <>
+      <CenterBlank>
+        <div className={errorStyles.imageWrapper}>
+          <Image src="/images/mirr/stand.png" alt="error" fill className={errorStyles.image} />
+        </div>
+        <div className={errorStyles.textWrapper}>
+          <Text.TitleH2>접근할 수 없는 페이지 입니다</Text.TitleH2>
+        </div>
+      </CenterBlank>
+      <BottomFixedButton>
+        <BottomFixedButton.First onClick={onClickHome} color="purple500-active">
+          <Text.ButtonL color={'white'}>홈으로 가기</Text.ButtonL>
+        </BottomFixedButton.First>
+      </BottomFixedButton>
+    </>
+  );
+};
+
+export default NotFoundErrorPage;
