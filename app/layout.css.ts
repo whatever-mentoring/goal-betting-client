9209235b733e185ad09d0c2ff006cfdcb19fd5b7
ww8007 <wshmin1234@gmail.com>
2023-12-23 08:23:37 +0900

    design: error 페이지 스타일 추가

--- app/layout.css.ts
+++ app/layout.css.ts
@@ -1,3 +1,30 @@
 import { style } from '@vanilla-extract/css';
+import { flexCenterCenter } from './common/ui/common.css';
+import getRem from './common/util/getRem';
 
 export const layoutStyle = style({});
+
+export const errorStyles = {
+  container: style({
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
+  imageWrapper: style({
+    position: 'relative',
+    width: '100vw',
+    height: 'calc(100vh / 3)',
+  }),
+  image: style({
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+  }),
+  textWrapper: style([flexCenterCenter, { marginTop: `${getRem(20)}` }]),
+
+  text: style({
+    fontSize: '3rem',
+  }),
+};
