ffc0089891c2ea054f869501ac6567bf6e043089
ww8007 <wshmin1234@gmail.com>
2023-12-23 06:55:23 +0900

    feat: 다짐 인증 전체 조회 api 및 useQuery 추가

---
+++ app/(challenge)/(다짐 도전)/module/api/certificateList.ts
@@ -0,0 +1,49 @@
+import client from '@/app/common/api/client';
+import { withSessionUser } from '@/app/common/api/withSessionUser';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+export interface Json {
+  isSuccess: boolean;
+  data: Data;
+  errorResponse: unknown;
+}
+export interface Data {
+  goalProofs: GoalProofs[];
+}
+export interface GoalProofs {
+  id: number;
+  userId: number;
+  goalId: number;
+  url: string;
+  comment: string;
+  progressDay: number;
+}
+
+interface RequestInterface {
+  goalId: number;
+}
+
+const getAPI = withSessionUser(async ({ goalId }: RequestInterface) => {
+  const { data } = await client<Json>({
+    method: 'get',
+    url: `/v1/api/goal/${goalId}/goal-proof`,
+  });
+  return data;
+});
+
+export interface GetAPIRequest {
+  goalId: number;
+  token?: string;
+}
+
+const GET_CERTIFICATE_LIST_KEY = (params: GetAPIRequest) => [
+  'GET_CERTIFICATE_LIST_KEY',
+  params.goalId,
+];
+
+export const useGETCertificateListQuery = (params: GetAPIRequest) => {
+  return useSuspenseQuery({
+    queryKey: GET_CERTIFICATE_LIST_KEY(params),
+    queryFn: () => getAPI(params),
+  });
+};
