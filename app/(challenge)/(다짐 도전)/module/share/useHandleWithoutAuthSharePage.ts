997eaee3622df726078bbf986ea6a0b086fba27b
ww8007 <wshmin1234@gmail.com>
2023-12-30 17:24:57 +0900

    feat: 미 로그인 사용자를 위한 공유 페이지 추가

---
+++ app/(challenge)/(다짐 도전)/module/share/useHandleWithoutAuthSharePage.ts
@@ -0,0 +1,96 @@
+'use client';
+
+import navigationPath from '@/app/common/navigation/navigationPath';
+import { useDrawer } from '@/app/common/ui/Drawer/DrawerContext';
+import {
+  getDayPeriodToText,
+  getLeftDaysFromDate,
+  isTodayIsAfterEndDate,
+} from '@/app/common/util/date';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { ParticipateButtonProps } from '../../ui/Share/CompoundSharePage';
+import { ChallengeDataWithoutAuth, useGetChallengeInfoWithoutTokenQuery } from '../api/challenge';
+
+interface HandleSharePageProps {
+  goalId: number;
+}
+
+const useHandleWithoutAuthSharePage = ({ goalId }: HandleSharePageProps) => {
+  const router = useRouter();
+  // SERVER
+  const [challengeInfo, setChallengeInfo] = useState<ChallengeDataWithoutAuth | null>(null);
+  const { data: challengeInfoData } = useGetChallengeInfoWithoutTokenQuery({
+    goalId,
+  });
+
+  // 1. 챌린지 정보 가져오기
+  useEffect(() => {
+    if (!challengeInfoData) return;
+    if (isTodayIsAfterEndDate(challengeInfoData.data.startDate)) {
+      router.push(navigationPath.다짐_페이지(goalId), {
+        scroll: false,
+      });
+      return;
+    }
+    if (challengeInfoData.data) {
+      setChallengeInfo(challengeInfoData.data);
+    }
+  }, [challengeInfoData]);
+
+  // 3. 참가자 정보 가져오기
+  // TODO : 서버에서 인증 정보 없어진 API 생기면 추가
+  const [participantInfo, setParticipantInfo] = useState<ParticipateButtonProps>({
+    overItemText: '',
+    buttonText: '',
+    onClick: () => {},
+    onClickOverItem: () => {},
+  });
+
+  useEffect(() => {
+    setParticipantInfo({
+      overItemText: `여러명이 참여중`,
+      buttonText: '참여하기',
+      onClickOverItem: onClickParticipateList,
+      onClick: onClickParticipate,
+    });
+  }, [challengeInfo]);
+
+  // 4. 참가하기
+  const onClickParticipateList = () => {
+    router.push(navigationPath.유저_목록_페이지(goalId), {
+      scroll: false,
+    });
+  };
+
+  const onClickParticipate = () => {
+    router.push(navigationPath.로그인_퍼널.로그인, {
+      scroll: false,
+    });
+  };
+
+  // UI LOGIC
+  const getLeftDays = () => {
+    if (!challengeInfo) return 0;
+    return getLeftDaysFromDate(challengeInfo.startDate);
+  };
+
+  const getPeriodText = () => {
+    if (!challengeInfo) return '';
+    return getDayPeriodToText(challengeInfo.startDate, 7);
+  };
+
+  // USER INTERACTION
+  // 모든 유저 > 상단 헤더 메뉴 클릭
+  const { openDrawer } = useDrawer();
+
+  return {
+    challengeInfo,
+    participantInfo,
+    getLeftDays,
+    getPeriodText,
+    openDrawer,
+  };
+};
+
+export default useHandleWithoutAuthSharePage;
