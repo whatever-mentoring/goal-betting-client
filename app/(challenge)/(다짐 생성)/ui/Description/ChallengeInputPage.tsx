4064e1dc5b6a16c4439516db772b2cb2cd0fd2d4
ww8007 <wshmin1234@gmail.com>
2023-12-30 17:35:43 +0900

    feat: 이미지 대신 박스 형식의 예시 챌린지로 변경 및 랜덤 추천 워딩 추가

--- app/(challenge)/(다짐 생성)/ui/Description/ChallengeInputPage.tsx
+++ app/(challenge)/(다짐 생성)/ui/Description/ChallengeInputPage.tsx
@@ -4,7 +4,7 @@ import Text from '@/app/common/ui/Text/Text';
 import TextArea from '@/app/common/ui/TextArea/TextArea';
 import { headerTextWrapper } from '@/app/common/ui/common.css';
 import Image from 'next/image';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useLayoutEffect, useState } from 'react';
 import { Challenge, ChallengeAddFunnelProps } from '../../add/page';
 import { challengeInputPageStyles } from './challengeInput.css';
 
@@ -13,11 +13,26 @@ interface DescriptionPageProps extends ChallengeAddFunnelProps {
   setChallenge: Dispatch<SetStateAction<Challenge>>;
 }
 
+const randomExample = [
+  '매일매일 헬스장 가기 이건 어때?',
+  '매일매일 책읽기 이건 어때?',
+  '매일매일 물 마시기 이건 어때?',
+] as const;
+
 const ChallengeInputPage = ({ challenge, setChallenge, onNext }: DescriptionPageProps) => {
   const isAllFilled = challenge.title.length > 0;
+
+  const [randomElement, setRandomElement] =
+    useState<(typeof randomExample)[number]>('매일매일 물 마시기 이건 어때?');
+
+  useLayoutEffect(() => {
+    const randomIndex = Math.floor(Math.random() * randomExample.length);
+    setRandomElement(randomExample[randomIndex]);
+  }, []);
+
   return (
     <>
-      <Header showBackButton />
+      <Header />
       <div className={headerTextWrapper}>
         <Text.TitleH1>어떤 다짐을 할거니?</Text.TitleH1>
       </div>
@@ -37,14 +52,22 @@ const ChallengeInputPage = ({ challenge, setChallenge, onNext }: DescriptionPage
           />
         </TextArea>
       </div>
-      <div className={challengeInputPageStyles.imageWrapper}>
-        <Image
-          className={challengeInputPageStyles.image}
-          src="/images/dog.png"
-          fill
-          alt="challenge_add"
-          priority
-        />
+      <div className={challengeInputPageStyles.boxCanvas}>
+        <div className={challengeInputPageStyles.boxWrapper}>
+          <div className={challengeInputPageStyles.imageWrapper}>
+            <Image
+              src="/images/mirr/mirr_Gifticon.png"
+              fill
+              alt="challenge-info"
+              className={challengeInputPageStyles.image}
+              sizes="(max-width: 480px) 142px"
+              priority
+            />
+          </div>
+          <div className={challengeInputPageStyles.challengeExampleWrapper}>
+            <Text.BodyS color="grey400">{randomElement}</Text.BodyS>
+          </div>
+        </div>
       </div>
       <BottomFixedButton>
         <BottomFixedButton.First
