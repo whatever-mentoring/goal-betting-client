5300f12631e6f4ef3228ac8c3cc15e0f51413386
ww8007 <wshmin1234@gmail.com>
2023-12-31 09:07:37 +0900

    fix: 성공 실패 여부에 따라 이미지 변경 되어야 하는 정책 반영

--- app/(challenge)/(다짐 결과)/module/result/ResultStepBuilder.ts
+++ app/(challenge)/(다짐 결과)/module/result/ResultStepBuilder.ts
@@ -13,6 +13,7 @@ type Link = {
 export interface BuilderStep {
   headerText: string;
   buttonText: string;
+  imageUrl: string;
   buttonTo?: Funnel | Link;
 }
 
@@ -20,9 +21,15 @@ interface ActionHandler {
   getSteps(): BuilderStep[];
 }
 
+type mirrImageType = '웃고있는미르' | '슬픈미르';
+
+const mirrImage: Record<mirrImageType, string> = {
+  웃고있는미르: '/images/mirr/mirr_happy.png',
+  슬픈미르: '/images/mirr/mirr_sad.png',
+} as const;
+
 const successText = (nickname: string) => `${nickname}님이\n다짐을 성공했어!`;
 const failText = (nickname: string) => `${nickname}님이\n다짐을 실패했어 ㅠㅠ`;
-
 const winnerText = (nickname: string) => `${nickname}님이\n당첨됐어`;
 
 class 주최자_다짐성공_기프티콘있음 implements ActionHandler {
@@ -32,6 +39,7 @@ class 주최자_다짐성공_기프티콘있음 implements ActionHandler {
     return [
       {
         headerText: successText(this.nickname),
+        imageUrl: mirrImage.웃고있는미르,
         buttonText: '기프티콘 가져가기',
         buttonTo: {
           type: 'funnel',
@@ -48,6 +56,7 @@ class 주최자_다짐성공_기프티콘없음 implements ActionHandler {
     return [
       {
         headerText: successText(this.nickname),
+        imageUrl: mirrImage.웃고있는미르,
         buttonText: '다음으로',
         buttonTo: {
           type: 'funnel',
@@ -67,10 +76,12 @@ class 주최자_다짐실패_기프티콘있음 implements ActionHandler {
     return [
       {
         headerText: failText(this.nickname),
+        imageUrl: mirrImage.슬픈미르,
         buttonText: '기프티콘 룰렛 돌리기',
       },
       {
         headerText: winnerText(this.winnerNickname),
+        imageUrl: mirrImage.웃고있는미르,
         buttonText: '다음으로',
         buttonTo: {
           type: 'funnel',
@@ -87,26 +98,11 @@ class 주최자_다짐실패_기프티콘없음 implements ActionHandler {
     return [
       {
         headerText: failText(this.nickname),
+        imageUrl: mirrImage.슬픈미르,
         buttonText: '다음으로',
         buttonTo: {
           type: 'funnel',
           step: '도전_결과_저장',
-        },
-      },
-    ];
-  }
-}
-
-class 참여자_다짐성공_기프티콘있음 implements ActionHandler {
-  constructor(private nickname: string) {}
-  getSteps(): BuilderStep[] {
-    return [
-      {
-        headerText: successText(this.nickname),
-        buttonText: '나도 내기 만들기',
-        buttonTo: {
-          type: 'link',
-          link: navigationPath.다짐_생성_퍼널.다짐_입력,
         },
       },
     ];
@@ -119,6 +115,24 @@ class 참여자_다짐성공_기프티콘없음 implements ActionHandler {
     return [
       {
         headerText: successText(this.nickname),
+        imageUrl: mirrImage.웃고있는미르,
+        buttonText: '나도 내기 만들기',
+        buttonTo: {
+          type: 'link',
+          link: navigationPath.다짐_생성_퍼널.다짐_입력,
+        },
+      },
+    ];
+  }
+}
+
+class 참여자_다짐실패_기프티콘_없음 implements ActionHandler {
+  constructor(private nickname: string) {}
+  getSteps(): BuilderStep[] {
+    return [
+      {
+        headerText: failText(this.nickname),
+        imageUrl: mirrImage.슬픈미르,
         buttonText: '나도 내기 만들기',
         buttonTo: {
           type: 'link',
@@ -135,10 +149,12 @@ class 참여자_다짐실패_기프티콘있음_당첨 implements ActionHandler 
     return [
       {
         headerText: failText(this.nickname),
+        imageUrl: mirrImage.슬픈미르,
         buttonText: '기프티콘 룰렛 돌리기',
       },
       {
         headerText: '기프티콘에 당첨됐어!',
+        imageUrl: mirrImage.웃고있는미르,
         buttonText: '기프티콘 받기',
         buttonTo: {
           type: 'link',
@@ -158,10 +174,12 @@ class 참여자_다짐실패_기프티콘있음_미당첨 implements ActionHandl
     return [
       {
         headerText: failText(this.nickname),
+        imageUrl: mirrImage.슬픈미르,
         buttonText: '기프티콘 룰렛 돌리기',
       },
       {
         headerText: winnerText(this.winnerNickname),
+        imageUrl: mirrImage.웃고있는미르,
         buttonText: '나도 내기 만들기',
         buttonTo: {
           type: 'link',
@@ -206,28 +224,37 @@ export class ResultStepBuilder {
   }
 
   build(): ActionHandler | null {
-    if (this.isOwner && this.isSuccess && this.hasGifticon) {
+    console.log(this);
+    if (this.isOwner && this.isSuccess && this.hasGifticon && !this.isWinner) {
+      console.log('주최자_다짐성공_기프티콘있음');
       return new 주최자_다짐성공_기프티콘있음(this.nickname);
     }
-    if (this.isOwner && this.isSuccess && !this.hasGifticon) {
+    if (this.isOwner && this.isSuccess && !this.hasGifticon && !this.isWinner) {
+      console.log('주최자_다짐성공_기프티콘없음');
       return new 주최자_다짐성공_기프티콘없음(this.nickname);
     }
-    if (this.isOwner && !this.isSuccess && this.hasGifticon) {
+    if (this.isOwner && !this.isSuccess && this.hasGifticon && !this.isWinner) {
+      console.log('주최자_다짐실패_기프티콘있음');
       return new 주최자_다짐실패_기프티콘있음(this.nickname, this.winnerNickname);
     }
-    if (this.isOwner && !this.isSuccess && !this.hasGifticon) {
+    if (this.isOwner && !this.isSuccess && !this.hasGifticon && !this.isWinner) {
+      console.log('주최자_다짐실패_기프티콘없음');
       return new 주최자_다짐실패_기프티콘없음(this.nickname);
     }
-    if (!this.isOwner && this.isSuccess && this.hasGifticon) {
-      return new 참여자_다짐성공_기프티콘있음(this.nickname);
+    if (!this.isOwner && !this.isSuccess && !this.hasGifticon && !this.isWinner) {
+      console.log('참여자_다짐실패_기프티콘_없음');
+      return new 참여자_다짐실패_기프티콘_없음(this.nickname);
     }
-    if (!this.isOwner && this.isSuccess && !this.hasGifticon) {
+    if (!this.isOwner && this.isSuccess && !this.hasGifticon && !this.isWinner) {
+      console.log('참여자_다짐성공_기프티콘없음');
       return new 참여자_다짐성공_기프티콘없음(this.nickname);
     }
     if (!this.isOwner && !this.isSuccess && this.hasGifticon && this.isWinner) {
+      console.log('참여자_다짐실패_기프티콘있음_당첨');
       return new 참여자_다짐실패_기프티콘있음_당첨(this.nickname);
     }
-    if (!this.isOwner && !this.isSuccess && !this.hasGifticon) {
+    if (!this.isOwner && !this.isSuccess && this.hasGifticon && !this.isWinner) {
+      console.log('참여자_다짐실패_기프티콘있음_미당첨');
       return new 참여자_다짐실패_기프티콘있음_미당첨(this.nickname, this.winnerNickname);
     }
     return null;
--- app/(challenge)/(다짐 결과)/ui/Result/ResultPage.tsx
+++ app/(challenge)/(다짐 결과)/ui/Result/ResultPage.tsx
@@ -1,9 +1,7 @@
 import { 다짐_결과_퍼널_Key } from '@/app/common/navigation/navigationPath';
 import TransitionComponent from '@/app/common/ui/Animated/TransitionComponent';
 import Header from '@/app/common/ui/Header/Header';
-import Image from 'next/image';
 import useHandleResult from '../../module/result/useHandleResult';
-import { resultPageStyles } from './result.css';
 
 interface ResultPageProps {
   setStep: (key: 다짐_결과_퍼널_Key) => void;
@@ -15,20 +13,7 @@ const ResultPage = ({ setStep, goalId }: ResultPageProps) => {
   return (
     <>
       <Header showBackButton />
-      {steps && (
-        <TransitionComponent steps={steps}>
-          <div className={resultPageStyles.imageWrapper}>
-            <Image
-              src="/images/dog.png"
-              width={100}
-              height={100}
-              layout="responsive"
-              alt="challenge-info"
-              className={resultPageStyles.image}
-            />
-          </div>
-        </TransitionComponent>
-      )}
+      {steps && <TransitionComponent steps={steps} />}
     </>
   );
 };
