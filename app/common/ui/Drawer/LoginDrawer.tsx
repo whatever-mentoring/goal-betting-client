e2624a8b3ddf96fdb4a8653928720ce3970da82a
ww8007 <wshmin1234@gmail.com>
2024-01-02 15:09:41 +0900

    design: 로그인 버튼 ui 변경

--- app/common/ui/Drawer/LoginDrawer.tsx
+++ app/common/ui/Drawer/LoginDrawer.tsx
@@ -2,7 +2,6 @@
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import navigationPath from '../../navigation/navigationPath';
-import Button from '../Button/Button';
 import Text from '../Text/Text';
 import Icon from '../assets/Icon';
 import { loginDrawerStyles } from './loginDrawer.css';
@@ -23,16 +22,17 @@ const LoginDrawer = () => {
           </>
         )}
         {!nickname && (
-          <Button color="purple500-active">
-            <Link
-              href={navigationPath.로그인_퍼널.로그인}
-              scroll={false}
-              className={loginDrawerStyles.loginButton}
-            >
-              <Icon name="docs" size="s" fill="white" />
-              <Text.BodyS color="white">로그인 하기</Text.BodyS>
-            </Link>
-          </Button>
+          <Link
+            href={navigationPath.로그인_퍼널.로그인}
+            scroll={false}
+            className={loginDrawerStyles.loginButton}
+          >
+            <div className={loginDrawerStyles.nicknameWrapper}>
+              <Text.TitleH2 color="cyan300">{'로그인 하기 :)'}</Text.TitleH2>
+              {/* <Text.TitleH2 color="white"></Text.TitleH2> */}
+            </div>
+            {/* <Text.BodyS color="grey400">카카오 로그인</Text.BodyS> */}
+          </Link>
         )}
       </div>
 
